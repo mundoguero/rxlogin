9a3703ddfd0622b9f730633d95c05fcf017c4ec1
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2020-11-17 15:14:54 -0300

    Initial Commit

---
+++ rxlogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxlogin
+//
+//  Created by Jonatas Brisotti on 17/11/20.
+//  Copyright © 2020 Jonatas Brisotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rxloginTests/rxloginTests.swift
@@ -0,0 +1,34 @@
+//
+//  rxloginTests.swift
+//  rxloginTests
+//
+//  Created by Jonatas Brisotti on 17/11/20.
+//  Copyright © 2020 Jonatas Brisotti. All rights reserved.
+//
+
+import XCTest
+@testable import rxlogin
+
+class rxloginTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
